7cd9831aadb4c03a333c19b538de755cc1be098c
Xenophon81 <pvdheuvel81@gmail.com>
2026-02-11 20:55:05 +0100

    1password added

--- install-joris-dev.sh
+++ install-joris-dev.sh
@@ -5,6 +5,21 @@ echo "=== Updating system ==="
 sudo apt update
 sudo apt upgrade -y
 
+
+echo "=== Installing 1Password ==="
+
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] \
+https://downloads.1password.com/linux/debian/amd64 stable main" | \
+sudo tee /etc/apt/sources.list.d/1password.list
+
+sudo apt update
+sudo apt install -y 1password
+
+
+
 echo "=== Installing base packages ==="
 sudo apt install -y \
     git \
